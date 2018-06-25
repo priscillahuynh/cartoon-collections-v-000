cfb6341a9ef4f5311e0e8bfb13b2c1205bee5858
Priscilla H <priscillamhuynh@gmail.com>
2018-06-25 13:32:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   i=0
   new_array=[]
   while i<array.length
-    array.each_with_index |name|
+    yield
     new_array<< " ${index}"+"${name}"
   end
   new_array
