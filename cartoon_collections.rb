e9b591035119dd3bcabae3e18ef80c663f99c7d4
Priscilla H <priscillamhuynh@gmail.com>
2018-06-25 13:42:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,8 @@ end# Your code here
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
