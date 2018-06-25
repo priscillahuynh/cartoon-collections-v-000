934deba727597d02bd097f9d9305d932a61c49b6
Priscilla H <priscillamhuynh@gmail.com>
2018-06-25 13:31:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,8 @@ def roll_call_dwarves(array)# code an argument here
   while i<array.length
     array.each_with_index |name|
     new_array<< " ${index}"+"${name}"
+  end
+  array
 end
 
 def summon_captain_planet# code an argument here
